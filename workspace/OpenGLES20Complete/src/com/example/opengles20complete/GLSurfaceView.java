a70ae56cba5e047bb5642cc21df677a8c38c5ead
Kacyn Fujii <kacyn_fujii@hmc.edu>
2013-02-21 14:49:14 -0800

    Added files to 3DGraphics

---
+++ workspace/OpenGLES20Complete/src/com/example/opengles20complete/GLSurfaceView.java
@@ -0,0 +1,9 @@
+package com.example.opengles20complete;
+
+public class GLSurfaceView {
+
+	public GLSurfaceView(OpenGLESTestComplete openGLESTestComplete) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
