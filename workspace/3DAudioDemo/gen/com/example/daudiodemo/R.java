7a52af6cdb7ff27035e8668fd6370fb92719c949
Hannah Kastein <hannah.kastein@gmail.com>
2013-03-31 19:06:23 -0700

    Added instructions with image

--- workspace/3DAudioDemo/gen/com/example/daudiodemo/R.java
+++ workspace/3DAudioDemo/gen/com/example/daudiodemo/R.java
@@ -11,17 +11,19 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int button_gametel=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int begin_button=0x7f070005;
-        public static final int cancel_button=0x7f070004;
-        public static final int connect_button=0x7f070003;
-        public static final int devices_radiogroup=0x7f070002;
-        public static final int intro_button=0x7f070001;
-        public static final int menu_settings=0x7f070006;
-        public static final int play_button=0x7f070000;
+        public static final int begin_button=0x7f070006;
+        public static final int button_gametel=0x7f070000;
+        public static final int cancel_button=0x7f070005;
+        public static final int connect_button=0x7f070004;
+        public static final int devices_radiogroup=0x7f070003;
+        public static final int intro_button=0x7f070002;
+        public static final int menu_settings=0x7f070007;
+        public static final int play_button=0x7f070001;
     }
     public static final class layout {
         public static final int activity_demo_graphics=0x7f030000;
@@ -36,17 +38,18 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int begin_button=0x7f04000b;
-        public static final int cancel_button=0x7f040010;
-        public static final int connect_button_connect=0x7f04000d;
-        public static final int connect_button_connected=0x7f04000f;
-        public static final int connect_button_connecting=0x7f04000e;
+        public static final int begin_button=0x7f04000c;
+        public static final int button_gametel_description=0x7f04000a;
+        public static final int cancel_button=0x7f040011;
+        public static final int connect_button_connect=0x7f04000e;
+        public static final int connect_button_connected=0x7f040010;
+        public static final int connect_button_connecting=0x7f04000f;
         public static final int instruct_message=0x7f040009;
-        public static final int intro_button=0x7f04000a;
+        public static final int intro_button=0x7f04000b;
         public static final int intro_message=0x7f040006;
         public static final int menu_settings=0x7f040001;
         public static final int paired_warning=0x7f040008;
-        public static final int play_button=0x7f04000c;
+        public static final int play_button=0x7f04000d;
         public static final int select_device=0x7f040007;
         public static final int title_activity_demo_graphics=0x7f040003;
         public static final int title_activity_instruct_screen=0x7f040005;
